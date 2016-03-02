232dc9fc6e67859d1ad11da44075ea403ecb6d21
Simon Appelt <aipt32@gmail.com>
2016-03-02 22:01:36 +0100

    ?

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ module.exports = {
   entry: './app/SipIt.ts',
   output: {
     path: __dirname,
+<<<<<<< Updated upstream
     filename: "./dist/bundle.js"
+=======
+    filename: "dist/bundle.js"
+>>>>>>> Stashed changes
   },
   resolve: {
     extensions: ['', '.js', '.ts']
